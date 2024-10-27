d50d1cd8a7359f414e7e15eaa3222ab5df897633
Kumaran S <kumaran.s@velammal.edu.in>
2024-10-27 19:00:11 +0530

    Uploaded java

---
+++ Java/JavaNet/Http_URL_Connection.java
@@ -0,0 +1,16 @@
+package JavaNet;
+
+import java.net.*;
+
+public class Http_URL_Connection{
+    public static void main(String[] args){
+        try{
+            URL url=new URL("https://github.com/MrKumaran/JavaProgramming");
+            HttpURLConnection huc=(HttpURLConnection)url.openConnection();
+            for(int i=1;i<=8;i++){
+                System.out.println(huc.getHeaderFieldKey(i)+" = "+huc.getHeaderField(i));
+            }
+            huc.disconnect();
+        }catch(Exception e){System.out.println(e);}
+    }
+}
