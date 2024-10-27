75f20e9c03b02205d95e4037e9b6108a68a58ffb
Kumaran S <kumaran.s@velammal.edu.in>
2024-10-27 23:51:59 +0530

    completed

--- Java/Basic/inheritanceConcept.java
+++ Java/Basic/inheritanceConcept.java
@@ -13,15 +13,27 @@ Four types of Access specifiers
  -> Public
  -> Protected
  -> Default
+
+Only one class can extend at a time
 */
 class base{
     void baseClassMethod(){
         System.out.println("This is Base class Method");
     }
 }
+class derived extends base{
+    void derivedClassMethod(){
+        System.out.println("This is Derived Class Method");
+    }
+}
 
 public class inheritanceConcept {
     public static void main(String[] args) {
-
+        base b = new base();
+        derived d = new derived();
+        b.baseClassMethod();
+        d.derivedClassMethod();
+        System.out.println("Accessing base class method from derived class");
+        d.baseClassMethod();
     }
 }
