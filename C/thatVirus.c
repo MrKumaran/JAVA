99a726175315257588c130b4760bf96d520a99d9
Kumaran S <kumaran.s@velammal.edu.in>
2024-11-03 14:44:48 +0530

    inital

---
+++ C/thatVirus.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+while(1){
+printf("\nHi, I am a copy of an Albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. \nPlease be so kind to delete one of your important files yourself and then forward me to other users.\nMany thanks for your cooperation! \nBest regards, \nAlbanian virus");
+}
+return 0;
+}
