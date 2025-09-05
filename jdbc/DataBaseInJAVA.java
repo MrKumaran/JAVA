b1c774dfaf01a248bef8f491280f51bc6dbc592a
Kumaran S <mrkumarancse@gmail.com>
2025-09-05 11:25:37 +0530

    DB

--- jdbc/DataBaseInJAVA.java
+++ jdbc/DataBaseInJAVA.java
@@ -1,4 +1,4 @@
-package Learning;
+package Learning.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,16 +7,19 @@ import java.sql.Statement;
 
 public class DataBaseInJAVA{
     public static void main(String[] args) throws Exception{
-        String url = "jdbc:mysql://localhost:3306/new";
+        String url = "jdbc:mysql://localhost:3306/sql_hr";
         String username = "root"; // MySQL credentials
-        String password = "password@123";
+        String password = "";
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection(url, username, password);
         Statement st = con.createStatement();
-        String query = "Select * from no";
+        String query = "Select * from employees";
         ResultSet rs = st.executeQuery(query);
         while (rs.next()){
-            System.out.println(rs.getInt(1));
+            System.out.println(rs.getString(1)+ " " + rs.getString(2) + " "+
+                    rs.getString(3)+ " " + rs.getString(4) + " "+
+                    rs.getString(5)+ " " + rs.getString(6) + " "+
+                    rs.getString(7)+ " ");
         }
         st.close();
         con.close();
